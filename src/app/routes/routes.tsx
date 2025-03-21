dcf3290b14c3493544d01745f1414577f4b9ccbc
Lucas Paes <lucas@Mac-Pro-de-Lucas.local>
2025-03-21 00:50:06 -0300

    adiciona template de checkout com componentes Info, InfoMobile, Review e AddressForm; atualiza rotas para incluir Checkout

--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -2,12 +2,15 @@ import { Routes, Route } from "react-router";
 import { Home } from "../../pages/home/home";
 import SignIn from '../../pages/sign-in.tsx/sign-in'
 import Dashboard from "../../pages/dashboard/Dashboard";
+import Checkout from "../../pages/checkout/checkout"
+
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route index element={<SignIn />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/checkout" element={<Checkout />} />
     </Routes>
   )
 }
