cde8d339fa2e8a1cf7bd9a0a930fe9d163da6397
Lucas Paes <lucas@Mac-Pro-de-Lucas.local>
2025-03-21 00:41:23 -0300

    adiciona novos componentes ao dashboard, incluindo MenuButton, Header, CardAlert e Search; atualiza rotas para incluir Dashboard e SignIn

--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router";
 import { Home } from "../../pages/home/home";
+import SignIn from '../../pages/sign-in.tsx/sign-in'
+import Dashboard from "../../pages/dashboard/Dashboard";
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route index element={<Home />} />
+      <Route index element={<SignIn />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   )
 }
